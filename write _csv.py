76037fce63eb9517e8bad6f88f3d5faa57271585
Ronen Lubin <63970571+ronen7822@users.noreply.github.com>
2021-09-07 20:27:21 +0300

    Update write _csv.py

--- write _csv.py
+++ write _csv.py
@@ -2,34 +2,28 @@ import csv
 import time
 import scrap
 
+head_line = ['city', 'price', 'area', 'house type', 'rooms', 'bars', 'furniture', 'lift',
+             'parking', 'air condition']
+
+
+# return row of data on given apartment
+def get_row(apartment):
+    row = [apartment.get_city(), apartment.get_price(), apartment.get_area(), apartment.get_house_type(),
+           apartment.get_rooms(), apartment.get_bars(), apartment.get_furniture(), apartment.get_lift(),
+           apartment.get_parking(), apartment.get_air_condition()]
+
+    return row
+
 
 # writes the apartments data to the csv file
 def write_to_csv():
-
     apartments_data = scrap.get_data()
-    print("finished to gather the data")
-
+    
     with open('apartments.csv', "w", newline='', encoding="utf-8") as csvfile:
         csv_writer = csv.writer(csvfile)
-        head_line = ['city', 'price', 'area', 'house type', 'rooms', 'bars', 'furniture', 'lift',
-                     'parking', 'air condition']
         csv_writer.writerow(head_line)
-
         for apartment in apartments_data:
-            row = []
-
-            row.append(apartment.get_city())
-            row.append(apartment.get_price())
-            row.append(apartment.get_area())
-            row.append(apartment.get_house_type())
-            row.append(apartment.get_rooms())
-            row.append(apartment.get_bars())
-            row.append(apartment.get_furniture())
-            row.append(apartment.get_lift())
-            row.append(apartment.get_parking())
-            row.append(apartment.get_air_condition())
-
-            csv_writer.writerow(row)
+            csv_writer.writerow(get_row(apartment))
 
 
 start_time = time.time()
